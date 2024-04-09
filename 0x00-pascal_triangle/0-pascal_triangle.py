1ab082751af9405ae8d6f51b581688679a01aebf
Layla Abkari <lolabkari@gmail.com>
2024-04-09 12:28:08 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "Pascal Triangle"
 
+
 def pascal_triangle(n):
     """Return the nth row of Pascals triangle as a list."""
     if n <= 0:
@@ -8,10 +9,9 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        l = [1] * (i + 1)
+        ligne = [1] * (i + 1)
         if i > 1:
             for j in range(1, i):
-                l[j] = triangle[i  - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(l)
-
+                ligne[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(ligne)
     return triangle
