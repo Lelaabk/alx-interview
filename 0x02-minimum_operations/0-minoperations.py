884122ca2a07dd801a77aa4c5cf981bbcbad8367
Layla Abkari <lolabkari@gmail.com>
2024-04-23 22:53:37 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Minoperation """
 
+
 def minOperations(n):
     """ Calculates the min number of operations needed to achieve n char."""
     if n <= 1:
         return 0
-    
+
     op = 0
     div = 2
 
