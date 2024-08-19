18255fff08c208df130c0a160b7245f36c53aa04
Layla Abkari <lolabkari@gmail.com>
2024-08-19 13:26:01 +0100

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Function that determines fewest number of coins
+needed to meet total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determine fewest number of coins needed to meet
+    given amount total
+    """
+    if total < 1:
+        return 0
+    
+    change = -1
+
+    if len(coins):
+        coins = sorted(coins, reverse=True)
+        noOfCoins = len(coins)
+        change = 0
+
+        for i in range(noOfCoins):
+            while total:
+                if total - coins[i] >= 0:
+                    change += 1
+                    total -= coins[i]
+                else:
+                    break
+
+        if total != 0:
+            change = -1
+    return change
